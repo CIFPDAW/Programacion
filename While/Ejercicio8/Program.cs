43c2a9485a4f89c8fbf24fff8e78ffbb1d888d4b
DanoGlez <fdariasglez@hotmail.com>
2024-10-18 12:51:47 +0100

    ADD Ejercicio 8

--- While/Ejercicio8/Program.cs
+++ While/Ejercicio8/Program.cs
@@ -1,14 +1,23 @@
 ﻿// (8) Imprimir y contar el número de múltiplos de tres que existen desde el 1 a un número solicitado por teclado.
 using System;
 
-public class Program{
-    public static void Main(string[] args){
-        int num, suma = 1;
-        Console.Write("Ingrese un número: ");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num, i = 1;
+        Console.Write("Ingresa un número: ");
         num = Convert.ToInt32(Console.ReadLine());
-        while (suma < num){
-            Console.WriteLine(suma);
-            suma += 1;
+
+        Console.WriteLine("Múltiplos de tres desde 1 hasta " + num + ":");
+        while (i <= num)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            i++;
         }
+
     }
 }
