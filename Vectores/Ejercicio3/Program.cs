1b0edb048b3b1d311944601d2ee592ff8f86f3a8
DanoGlez <fdariasglez@hotmail.com>
2024-10-30 12:33:10 +0000

    Ejercicios Vectores
    Me cago en los muertos del barrigudo, no estan acabados

---
+++ Vectores/Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿// (3) Solicitar 10 números distintos por teclado y mostrarlos.
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] lista = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Dame un numero: ");
+            lista[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($" {lista[i]}");
+        }
+    }
+}
---
+++ Vectores/Ejercicio5/Program.cs
@@ -0,0 +1,27 @@
+﻿// (5) Crear un programa que pida al usuario 10 números decimales, calcule su media y luego muestre los que están por encima de la media.
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        decimal suma, max = 10;
+        decimal[] lista = new decimal[max];
+
+        for (int i = 0; i < max; i++)
+        {
+            Console.Write("Introduce un número: ");
+            lista[i] = Convert.ToInt32(Console.ReadLine());
+            suma += lista[i];
+        }
+
+        decimal resultado = suma / max;
+
+        for (int i = 0; i < max; i++)
+        {
+            if(lista[i] > resultado){
+                Console.Write($" {lista[i]}");
+            }
+        }
+    }
+}
