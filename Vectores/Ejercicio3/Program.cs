fdd4881be2f8cae60360f98cc9b34d17f7101ef6
DanoGlez <fdariasglez@hotmail.com>
2024-11-06 09:29:39 +0000

    FIX Repo
    Ahora solamente estan los .cs necesarios. El resto fue todo borrado ;)

--- Vectores/Ejercicio3/Program.cs
+++ Vectores/Ejercicio3/Program.cs
@@ -9,9 +9,29 @@ public class Program
         int[] lista = new int[10];
         for (int i = 0; i < 10; i++)
         {
-            Console.Write("Dame un numero: ");
-            lista[i] = Convert.ToInt32(Console.ReadLine());
+            int numero;
+            bool esDistinto;
+            do
+            {
+                esDistinto = true;
+                Console.Write("Dame un numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                // Verificar si el número ya está en la lista
+                for (int j = 0; j < i; j++)
+                {
+                    if (lista[j] == numero)
+                    {
+                        esDistinto = false;
+                        Console.WriteLine("El número ya ha sido ingresado. Intenta con otro número.");
+                    }
+                }
+            } while (!esDistinto);
+
+            lista[i] = numero;
         }
+
+        Console.WriteLine("Números ingresados:");
         for (int i = 0; i < 10; i++)
         {
             Console.Write($" {lista[i]}");
