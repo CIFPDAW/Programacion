af92ea63f2c193c81bf6a99d6781cd41ef4c0a6e
DanoGlez <fdariasglez@hotmail.com>
2024-11-14 12:17:56 +0000

    ADD Ejercicios con métodos

--- Strings/Métodos/Ejercicio3/Program.cs
+++ Strings/Métodos/Ejercicio3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dada una cadena de caracteres que contenga el nombre de una persona en formato
+//  APELLIDOS, NOMBRE, convertirla en una cadena del tipo NOMBRE APELLIDOS.
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string cadena;
+        Console.Write("Coloca tu nombre en formato APELLIDOS, NOMBRE: ");
+        cadena = Console.ReadLine();
+        string[] nombre = cadena.Split(", ");
+        Console.WriteLine(nombre[1] + " " + nombre[0]);
+    }
+}
