e1c772e999d1289023fffa74fa6def913d8ad54d
DanoGlez <fdariasglez@hotmail.com>
2025-02-05 11:25:05 +0000

    add: examen
    - Se reorganizaron las carpetas de exámenes.

---
+++ Entregas/MiniExamenT2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Realiza la tarea que se encomienda.  Sin usar documentación. 
+
+* Implementa una función:  public static bool comprobarPassword(String pass)
+   …  que devolverá true si la contraseña pasada como parámetro cumple las condiciones:
+
+ Tiene entre 4 y 6 caracteres.
+ Contiene un dígito
+
+
+
+De la clase Char:
+public static bool IsDigit (char c);*/
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args){
+        Console.WriteLine(comprobarPassword("Contaa"));
+    }
+
+    public static bool comprobarPassword(string pass)
+    {
+
+        if (pass.Length >= 4 && pass.Length <= 6)
+        {
+            for (int i = 0; i < pass.Length; i++)
+            {
+                char c = Convert.ToChar(pass[i]);
+                if (Char.IsDigit(c)) return true;
+            }
+        }
+
+        return false;
+    }
+}
