2f370aba6524fbca4b4ae86b4703cdbb9032e38c
DanoGlez <fdariasglez@hotmail.com>
2025-02-05 09:30:46 +0000

    pre examen

---
+++ Objetos/Punto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Puntos p = new Puntos(11, 8);
+        Puntos q = new Puntos();
+        q.x = 14;
+        q.y = 11;
+        p.Mostrar('.');
+        Console.WriteLine($"\np = {p}");
+        Console.WriteLine($"q = {q}");
+        Console.WriteLine($"dist(p,q) = {p.distancia(q)}");
+    }
+}
